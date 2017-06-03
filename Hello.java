e83035f5fb557b772924458afb02368d7ebd2e97
sanyabeast <a.gvrnsk@gmail.com>
2017-06-03 18:39:12 +0300

    gitignore, few classes in different files.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
+	static String str = "KEK";
 	public static void main(String[] args){
-		System.out.println("Hello");
+		System.out.println(str);
 		Element el = new Element("Table");
 
 		System.out.println(el);
