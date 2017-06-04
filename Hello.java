847f19d13ef1d6c7ed5edc4081cf77afb834c19b
sanyabeast <a.gvrnsk@gmail.com>
2017-06-04 23:48:33 +0300

    same name non-public classes test

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
+import tools.Helper;
 import tools.Element;
 
 public class Hello {
 	public static void main(String[] args){
-
+			Element el = new Element("thing");
+			el.logChildren();
 	}
 }
---
+++ tools/Helper.java
@@ -0,0 +1,11 @@
+package tools;
+
+public class Helper {
+
+};
+
+class Child {
+  public void logChildren(){
+    System.out.println("j");
+  }
+}
