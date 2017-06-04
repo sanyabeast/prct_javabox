39e79240d487b3c4f4175a672653a47725801a89
sanyabeast <a.gvrnsk@gmail.com>
2017-06-04 21:45:22 +0300

    git cache cleaned; class files successfully added 2 ignore.

--- Hello.java
+++ Hello.java
@@ -1,14 +1,7 @@
 import tools.Element;
 
 public class Hello {
-	static String str = "KEK";
 	public static void main(String[] args){
-		System.out.println(str);
-		Element el = new Element("table");
-
-		System.out.println(el);
-
-		el.logChildren();
 
 	}
 }
